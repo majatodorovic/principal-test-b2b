46716d3492463c75dfce0e0f818c24415680d747
Marija Todorovic <maja.todorovic2111@gmail.com>
2025-02-26 15:56:44 +0100

    123654789Test

--- app/[lang]/components/Navigation.tsx
+++ app/[lang]/components/Navigation.tsx
@@ -71,10 +71,10 @@ export default function Navigation({ data, lang }: any) {
             },
           )}
         </div>
-        <Link
+         <Link
           href="https://b2b.principal.croonus.com/prijava" 
           className="hidden items-center space-x-2 sm:flex"
-        >
+         >
           <Image
             src="/images/navigation/b2b.png"
             alt="B2B"
@@ -193,10 +193,10 @@ export default function Navigation({ data, lang }: any) {
         </div>
 
         <div className="mt-6 border-t pt-6">
-        <Link 
-          href="https://b2b.principal.croonus.com/prijava" 
-          className="xl:hidden"
-        >
+          <Link 
+            href="https://b2b.principal.croonus.com/prijava" 
+            className="xl:hidden"
+          >
             <div className="mt-4 flex items-center gap-2">
               <Image
                 src="/images/navigation/b2b.png"
